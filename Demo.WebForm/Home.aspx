5aeead219d697350a385c6160a7f763cbe95f539
Jo4D <yusufhasan182@gmail.com>
2023-02-19 18:53:00 +0200

    first commit

---
+++ Demo.WebForm/Home.aspx
@@ -0,0 +1,22 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Home.aspx.cs" Inherits="Demo.WebForm.Home" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+        </div>
+        <asp:DetailsView ID="DetailsView1" runat="server" BackColor="#DEBA84" BorderColor="#DEBA84" BorderStyle="None" BorderWidth="1px" CellPadding="3" CellSpacing="2" Height="152px" Width="608px">
+            <EditRowStyle BackColor="#738A9C" Font-Bold="True" ForeColor="White" />
+            <FooterStyle BackColor="#F7DFB5" ForeColor="#8C4510" />
+            <HeaderStyle BackColor="#A55129" Font-Bold="True" ForeColor="White" />
+            <PagerStyle ForeColor="#8C4510" HorizontalAlign="Center" />
+            <RowStyle BackColor="#FFF7E7" ForeColor="#8C4510" />
+        </asp:DetailsView>
+    </form>
+</body>
+</html>
---
+++ Demo.WebForm/Home.aspx.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Demo.WebForm
+{
+    public partial class Home : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+           var datalink = ConfigurationManager.ConnectionStrings["DBWF"].ConnectionString;
+            using (SqlConnection connection = new SqlConnection(datalink))
+            {
+                #region ExcuteReader
+                SqlCommand command = new SqlCommand();
+                command.CommandText = "Select * From Book";
+                command.Connection = connection;
+                connection.Open();
+                SqlDataReader reader = command.ExecuteReader();
+                DetailsView1.DataSource = reader;
+                DetailsView1.DataBind();
+                #endregion
+
+                // SqlCommand sqlCommand = new SqlCommand();
+                // sqlCommand.Connection = connection;
+                // sqlCommand.CommandText = "Select count(BookId) From Book";
+                // connection.Open();
+                //int TotalRows = (int)sqlCommand.ExecuteScalar();
+                // Response.Write($"The total rows is {TotalRows}");
+
+            }
+
+
+        }
+    }
+}
